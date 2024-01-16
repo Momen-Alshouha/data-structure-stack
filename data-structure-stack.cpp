ad3fb3e06ff5768d894b05ca81d4a383183c3278
momen-alshouha <momenalshouha@outlook.com>
2024-01-16 20:04:46 +0300

    -

--- data-structure-stack.cpp
+++ data-structure-stack.cpp
@@ -4,15 +4,6 @@
 
 int main()
 {
-	Stack<int> Stack;
-
-	Stack.Push(1);
-	Stack.Push(2);
-	Stack.Push(3);
-	cout <<"Top Item : "<< Stack.top << endl;
-	Stack.Clear();
-	Stack.Print();
-	cout << boolalpha<<Stack.IsExist(1) << endl;;
-	cout << "Stack Size : " << Stack.size << endl;
+	
 
 }
